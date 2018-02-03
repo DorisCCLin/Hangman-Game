7ddd661f655b87b285e1e8b5aa9cbe2b448a23a5
DorisCCLin <doriscclin@gmail.com>
2018-02-03 00:10:10 -0500

    game function added

--- index.js
+++ index.js
@@ -9,71 +9,122 @@ var randomAns = answers[Math.floor(Math.random()* answers.length)];
 console.log(randomAns)
 
 var letters = document.getElementById('letters')
-var used1 = document.getElementById('used1')
-var used2 = document.getElementById('used2')
-var used3 = document.getElementById('used3')
-var used4 = document.getElementById('used4')
-var used5 = document.getElementById('used5')
 var keyBoard = document.getElementById('keyBoard')
+
+
 
 for(var i=0;i<randomAns.length;i++){
 	var  para = document.createElement('p')
 	var dashes = document.createTextNode("_")
 	para.appendChild(dashes);
 	letters.appendChild(para);
+	para.setAttribute('class','paras')
 }
 
-var array = randomAns.split('')
+var paras = document.getElementsByClassName('paras');
 
+var array = randomAns.toUpperCase().split('')
 
-// function check()
+var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z']
 
-// document.addEventListener('keypress',function(e){
-// 	for (var i=0;i<used.length;i++){
-// 		used[iidf].innerHTML = e.key  
-// 	}  
-// })
-
-
-for(i=1;i<=26;i++){
+for(i=0;i< alpha.length;i++){
 var key = document.createElement('div')
+var ABC = document.createTextNode(alpha[i])
 	key.style.width='50px'
 	key.style.height="50px"
 	key.style.border='1px solid'
 	key.style.display = 'inline-block'
+	key.style.margin = '10px'
 	keyBoard.appendChild(key)
+	key.appendChild(ABC)
 	key.setAttribute('class','keys')
 }
 
-keys = document.getElementsByClassName('keys');
-
-var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z']
-
-for(i=0;i<26;i++){
-   keys[i].innerHTML = alpha[i]
-}
+var keys = document.getElementsByClassName('keys');
 
 
-for(let i=0;i<keys.length;i++){
-	keys[i].addEventListener('click', function(){
-		var text = keys[i].textContent;
-		console.log(text);
-	})
 
-}
-
-// for(let i=0;i<used.length;i++){
-// 	used[i].addEventListener('click', function(){
-// 		var text = used[i].textContent;
-// 		console.log(text);
-// 	})
-
+// for(i=0;i<26;i++){
+//    keys[i].innerHTML = alpha[i]
 // }
 
-// var b=a.match(/[e]+/g) // out put b= 2 e
+var clicks = 0;
+for(let i=0;i<keys.length;i++){
+	keys[i].addEventListener('click', function(){
+		var text = keys[i].textContent;	
+		console.log(text)
+		check(text);	
+	})
+}
 
-// class hangMan{
+var arr = []
+var arrUsed = []
+var msg = document.getElementById('message');
+var bubble = document.getElementById('bubble');
 
-// } 
+function check(letter){
+var found = 0
+clicks += 1;
+for (var i=0; i<array.length; i++){
+	if (letter === array[i]){
+			paras[i].innerText = letter
+			found++
+			arr.push(paras[i].innerHTML)
+			arrUsed.push(letter)			
+			bubble.innerHTML = 'Great! keep going!'
+			winning();
+			clickCount()
+			if (clicks >=5){
+			bubble.innerHTML = 'You lose'
+			}							
+		}	 
+	}							
+	
+if (found === 0) {
+	if (arrUsed.includes(letter) === true){
+			clicks -= 1;
+			bubble.innerHTML = 'You have guessed '+ letter	
+	} else {		
+			bubble.innerHTML = 'opps, try again!'			
+			arrUsed.push(letter)
+			winning();
+			clickCount();
+			if (clicks >=5){
+			bubble.innerHTML = 'You lose'}
+		}
+	} 
+}
+
+function clickCount(){	
+	if (clicks < 5){
+		msg.innerHTML = 'You still have '+ (5-clicks)+ ' lives!'
+	} else if (click = 5){
+		msg.innerHTML = 'You no more lives!'		
+	} 
+}
+
+
+function winning(){ 
+ if (arr.length === array.length){	
+	var newArray = array.slice().sort()
+	var newArr = arr.slice().sort()
+	if ( arr.toString() === array.toString()) {
+		winbox()
+	}
+   }	
+}
+
+var win = document.getElementById('win');
+var replay = document.getElementById('reload');
+
+function winbox(){	
+	win.style.display = "block";
+	replay.addEventListener('click', function(){
+		location.reload();
+	})
+}
+
+
+
 
 
