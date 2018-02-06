b1f25d6df86c9bcd1c372b36802f0c4477608471
DorisCCLin <doriscclin@gmail.com>
2018-02-05 23:50:42 -0500

    style finished

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-var answers= ['eggs','banana','happy','opt','fox','range']
+var answers= ['eggs','banana','happy','opt','fox','range','choice','love','hate','manipulate','peanut','knight','dream','sheet','book','star','galaxy']
 var randomAns = answers[Math.floor(Math.random()* answers.length)];
 console.log(randomAns)
 
 var letters = document.getElementById('letters')
 var keyBoard = document.getElementById('keyBoard')
 
+
+var arr = []
+var arrUsed = []
+var msg = document.getElementById('message');
+var bubble = document.getElementById('bubble');
+var mario = document.getElementById('mario');
 
 
 for(var i=0;i<randomAns.length;i++){
@@ -20,7 +26,7 @@ var paras = document.getElementsByClassName('paras');
 
 var array = randomAns.toUpperCase().split('')
 
-var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z']
+var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 for(i=0;i< alpha.length;i++){
 var key = document.createElement('div')
@@ -33,57 +39,60 @@ var ABC = document.createTextNode(alpha[i])
 var keys = document.getElementsByClassName('keys');
 
 var clicks = 0;
+
 for(let i=0;i<keys.length;i++){
 	keys[i].addEventListener('click', function(){
+		mario.style.webkitAnimationPlayState = "running"
 		var text = keys[i].textContent;	
 		console.log(text)
-		check(text);
-		mario.style.animationPlayState ='running';
-		mario.style.animationPlayState ='initial';
-
+		check(text);				
 	})
 }
 
-var arr = []
-var arrUsed = []
-var msg = document.getElementById('message');
-var bubble = document.getElementById('bubble');
-var mario = document.getElementById('mario');
 
-function check(letter){
-var found = 0
-clicks += 1;
-for (var i=0; i<array.length; i++){
-	mario.style.animationPlayState ='initial';
-	if (letter === array[i]){
-			paras[i].innerText = letter
-			found++
-			arr.push(paras[i].innerHTML)
-			arrUsed.push(letter)			
-			bubble.innerHTML = 'Great! keep going!'
-			winning();
-			clickCount()
-			if (clicks >=5){
-			bubble.innerHTML = 'You lose'
-			}							
-		}	 
-	}							
-	
-if (found === 0) {
-	mario.style.animationPlayState ='initial';
-	if (arrUsed.includes(letter) === true){
-			clicks -= 1;
-			bubble.innerHTML = 'You have guessed '+ letter	
-	} else {		
-			bubble.innerHTML = 'opps, try again!'			
-			arrUsed.push(letter)
-			winning();
-			clickCount();
-			if (clicks >=5){
-			bubble.innerHTML = 'You lose'}
-		}
-	} 
+function anime(){
+    if (mario.style.AnimationPlayState == "running"){
+    	mario.style.AnimationPlayState = "pause"
+    }   
 }
+
+anime();
+
+
+
+function check(letter){	
+var found = 0
+if (clicks <= 5){
+	if (arrUsed.includes(letter) === false){
+		arrUsed.push(letter)
+		for (var i=0; i<array.length; i++){
+			if (letter === array[i]){
+				paras[i].innerText = letter
+				found++
+				arr.push(paras[i].innerHTML)
+				bubble.innerHTML = 'Great! keep going!'
+				clickCount()
+				winning();
+			} 
+		}
+		if (found === 0) {
+			clicks += 1
+			if (clicks < 5){
+			bubble.innerHTML = 'opps, try again!'
+			clickCount()
+			} else{
+				losebox()			
+			} 	
+		}	
+	} else {
+		clicks -= 1;
+		bubble.innerHTML = 'You have guessed '+ letter	
+	}
+} else {
+	losebox()
+}	 
+}
+
 
 function clickCount(){	
 	if (clicks < 5){
@@ -98,21 +107,32 @@ function winning(){
  if (arr.length === array.length){	
 	var newArray = array.slice().sort()
 	var newArr = arr.slice().sort()
-	if ( arr.toString() === array.toString()) {
+	if ( newArr.toString() === newArray.toString()) {
 		winbox()
-	}
-   }	
+  }
+}
 }
 
 var win = document.getElementById('win');
-var replay = document.getElementById('reload');
+var replay_w = document.getElementById('reload_w');
 
 function winbox(){	
 	win.style.display = "block";
-	replay.addEventListener('click', function(){
+	replay_w.addEventListener('click', function(){
 		location.reload();
 	})
 }
+
+var lose = document.getElementById('lose');
+var replay_l = document.getElementById('reload_l');
+
+function losebox(){	
+	lose.style.display = "block";
+	replay_l.addEventListener('click', function(){
+		location.reload();
+	})
+}
+
 
 
 
